5eb1d7bc6f2cbd988745466d297230b010418627
llxal <18983679028@163.com>
2015-12-11 21:59:20 +0800

    fuck

---
+++ app/src/main/java/cn/alien95/phonebook/main/SingleFragmentActivity.java
@@ -0,0 +1,13 @@
+package cn.alien95.phonebook.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SingleFragmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_fragment);
+    }
+}
